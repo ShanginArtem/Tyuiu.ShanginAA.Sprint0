e45aabb1925f0f5423004ca5f1be598f21315ec9
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-09-18 02:20:19 +0500

    Task5

---
+++ Tyuiu.ShanginAA.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ShanginAA.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+          if (b==0)
+            {
+                Console.WriteLine("Ha 0 Delit nelzya", b);
+                return -1;
+            }
+          else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.ShanginAA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShanginAA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShanginAA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("999304a8-1864-4613-a3cd-61d74c84e04b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
